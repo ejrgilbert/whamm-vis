3294a2af8d24b8ad45588f57b58d0f068ac9c754
Gabe Small <gabe@gabrielsmall.com>
2025-07-24 16:15:55 -0400

    Sanitizes watFormatter Input

--- whamm-visualizer/src/wat_parser/watFormatter.ts
+++ whamm-visualizer/src/wat_parser/watFormatter.ts
@@ -7,7 +7,14 @@ import wabt from 'wabt';
  */
 export async function formatWatToWat(wat: string): Promise<string> {
     const wabtModule = await wabt();
-    const wasmModule = wabtModule.parseWat('reformat.wat', wat);
+    // The wabt parser is strict. Some tools may generate WAT with invalid identifiers,
+    // such as `$"#func179 dummy"`. This is a workaround to sanitize known invalid
+    // patterns before parsing. The ideal solution is to fix the WAT source.
+    const sanitizedWat = wat.replace(/\$"\#([^"]+)"/g, (match, group1) => {
+        // Replace the invalid space character with an underscore.
+        return `$${group1.replace(/\s/g, '_')}`;
+    });
+    const wasmModule = wabtModule.parseWat('reformat.wat', sanitizedWat, {annotations: true});
     wasmModule.validate();
     return wasmModule.toText({ foldExprs: false, inlineExport: false });
 }
@@ -23,3 +30,6 @@ export async function formatWasmToWat(wasm: Buffer | Uint8Array): Promise<string
     wasmModule.validate();
     return wasmModule.toText({ foldExprs: false, inlineExport: false });
 }
+
+
+// NOTE/TODO FSM Reorganizes when calculating, breaking line/fidpc pairing
